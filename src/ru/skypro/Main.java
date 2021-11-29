3d9ab5c0e2c65e3262307530e14a38d46372403a
Александр <sashaneiv01@yandex.ru>
2021-11-29 22:35:34 +0300

    Commit 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,19 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+	int[] firstArray= new int[3];
+	    firstArray[0] = 1;
+        firstArray[1] = 2;
+        firstArray[2] = 3;
+
+    double[] secondArray = {1.57, 7.654, 9.986 };
+
+    char[] thirdArray = {11, 22, 33};
+
+    //System.out.println(firstArray[0] + ", " + firstArray[1] + ", " + firstArray[2]);
+    //System.out.println(secondArray[0] + ", " + secondArray[1] + ", " + secondArray[2]);
+    //System.out.println(thirdArray[0] + ", " + thirdArray[1] + ", " + thirdArray[2]);
+
     }
 }
